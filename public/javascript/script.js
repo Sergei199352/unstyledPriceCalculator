6319ca2e7bd401229280c728d86c0f38f94606a8
sergey <s.stanislavchuk@rgu.ac.uk>
2024-02-23 13:05:35 +0000

    hello

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -13,3 +13,31 @@ function calculatePrice() {
   function roundToNearest50(num) {
     return Math.round(num / 50) * 50;
 }
+function storeQuote() {
+  var quoteName = document.getElementById('quoteName').value;
+  var salary = document.getElementById('salary').value;
+  var days = document.getElementById('days').value;
+
+  // Data to be sent in the POST request
+  var data = {
+      quoteName: quoteName,
+      salary: salary,
+      days: days
+  };
+
+  // Make a POST request to the server
+  $.ajax({
+      type: 'POST',
+      url: '/storequote',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      success: function(response) {
+          console.log('Quote Name:', response);
+          alert('Quote stored successfully with name: ' + response);
+      },
+      error: function(xhr, status, error) {
+          console.error('Error storing quote:', error);
+          alert('Error storing quote. Please try again.');
+      }
+  });
+}
--- server.js
+++ server.js
@@ -1,13 +1,20 @@
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var express  = require('express');
-var app =  express();
+const app = express();
 app.use(express.static('public'));
+app.use(bodyParser.json()); // Middleware to parse JSON request bodies
 
-app.use(function(req,res,next){
-    res.status(404).send('that went wronmg')
-})
-app.listen(8080, ()=>{
+app.post('/storequote', (req, res) => {
+    const { quoteName, salary, days } = req.body;
+    console.log('Received data from frontend:', quoteName, salary, days);
+    res.send(quoteName); // Just sending back the quoteName as a response for now
+});
 
-    console.log('server Running on the 8080 port')
+app.use(function(req, res, next) {
+    res.status(404).send('Oops! Something went wrong...');
+});
 
-})
+app.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
